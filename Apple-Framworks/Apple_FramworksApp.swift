8d219fb8a5b83bb6400abdb9ae78a22902f6e429
Hadeel Jamoul <hadeelgamol@gmail.com>
2024-12-14 16:18:50 +0300

    Initial Commit

---
+++ Apple-Framworks/Apple_FramworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FramworksApp.swift
+//  Apple-Framworks
+//
+//  Created by Hadeel Jamoul on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FramworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
