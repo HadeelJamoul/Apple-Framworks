180b70a418e08e14b88cf8a95ac5466fd482eb95
Hadeel Jamoul <hadeelgamol@gmail.com>
2024-12-16 14:23:16 +0300

    add images to assets, create FrameworkTitleView struct

---
+++ Apple-Framworks/FrameworkGridView.swift
@@ -0,0 +1,45 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Framworks
+//
+//  Created by Hadeel Jamoul on 14/12/2024.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        VStack{
+            FrameworkTitleView(imageName: "app-clip", name: "App Clips")
+        }
+    }
+}
+
+
+struct FrameworkTitleView : View {
+    
+    let imageName: String
+    let name: String
+    
+    var body: some View{
+        VStack(){
+            Image(imageName)
+                .resizable()
+                .frame(width: 100 , height: 100)
+
+            Text(name)
+                .font(.title2)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
+                // shrink the text if needed to 50 %
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
+        }
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
+
